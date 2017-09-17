cf091308b8a57b9f0df65fdd4e4356e940b4ad73
samonin <samonin@MacBook-Air-samonin.local>
2017-09-18 00:17:22 +0300

    add test controller main

---
+++ application/controllers/Main.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: samonin
+ * Date: 18.09.17
+ * Time: 00:14
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Main extends CI_Controller {
+    public function index()
+    {
+        echo "Works!";
+    }
+}
