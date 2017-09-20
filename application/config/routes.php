d0a122b50deeada4f9288e74705824ce23ff05b5
samonin <samonin@MacBook-Air-samonin.local>
2017-09-20 21:34:59 +0300

    routes isoft

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,5 +51,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['default_controller'] = 'main';
 //$route['isoft/(:any)'] = 'isoft/main/$1';
+$route['isoft/main'] = 'isoft/main';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
