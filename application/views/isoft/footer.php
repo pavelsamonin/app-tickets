3fc2fcae1717accdd5f09b54be3139ead3c6a466
samonin <samonin@MacBook-Air-samonin.local>
2017-09-22 15:33:02 +0300

    Popper.js_2

--- application/views/isoft/footer.php
+++ application/views/isoft/footer.php
@@ -6,7 +6,8 @@
  * Time: 13:03
  */
 ?>
-<script src="/assets/jquery-3.2.1.min.js"></script>
+<script src="/assets/js/jquery-3.2.1.min.js"></script>
+<script src="/assets/js/popper.min.js"></script>
 <script src="/assets/bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
