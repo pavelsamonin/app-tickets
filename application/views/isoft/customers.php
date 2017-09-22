6912fc4e840b3a7ca588920a74d3b5a3e1563f0b
samonin <samonin@MacBook-Air-samonin.local>
2017-09-22 14:32:48 +0300

    test4

--- application/views/isoft/customers.php
+++ application/views/isoft/customers.php
@@ -8,9 +8,11 @@
 ?>
 <div class="col-lg-8 col-lg-offset-2">
 
-    <h1><?php echo @$data['head']; ?></h1>
+    <h1><?php
+        var_dump($data);
+        echo @$data['head']; ?></h1>
 
-
+<?php die; ?>
     <table class="table">
         <thead class="thead-inverse">
         <tr>
