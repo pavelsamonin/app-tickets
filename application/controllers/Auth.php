eb8dbcb58146673ae8d43a9325f90e3ef739c0e1
samonin <samonin@MacBook-Air-samonin.local>
2017-09-25 22:25:12 +0300

    config controller2

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -7,16 +7,17 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-require_once ('Rest.php');
-class Auth extends CI_Controller
+require_once APPPATH . 'controllers/Rest.php';
+class Auth extends Rest
 {
+    var $rest;
     function __construct()
     {
         parent::__construct();
         $this->load->model('misoft');
         $this->load->model('mauth');
         $this->load->library('jwt');
-        $this->rest = new Rest;
+//        $this->rest = new Rest;
     }
 
     public function __call($method, $args)
